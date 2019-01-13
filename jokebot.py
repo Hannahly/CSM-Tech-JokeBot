587ce541137cda5c80746cc6d01b995c7e9d1d29
Hannahly <hazanna.23@gmail.com>
2019-01-13 01:42:25 -0600

    committing jokebot v1

---
+++ jokebot.py
@@ -0,0 +1,78 @@
+import time
+import csv
+import sys
+import os
+import requests
+import json
+
+class Jokebot:
+
+    def __init__(self, file = None):
+        if file == None:
+            self.joke_list = self.use_reddit()
+        else:
+            self.joke_list = self.read_csv(file)
+
+        self.run_jokebot()
+
+
+    def tell_joke(self,prompt, punchline):
+            print(prompt)
+            time.sleep(2)
+            print(punchline)
+
+    def read_input(self):
+        user_input = input()
+        if user_input == "next":
+            return True
+        elif user_input == "quit":
+            return False
+        else:
+            print('Please type either "next" or "quit"!')
+            return self.read_input()
+
+    def read_csv(self,csv_file):
+        joke_list = []
+        with open(os.path.dirname(os.path.abspath(__file__)) + "/" + csv_file) as f:
+            reader = csv.reader(f)
+            for line in reader:
+                joke_list.append(line)
+        return joke_list
+
+    def run_jokebot(self):
+        joke_spitter = iter(self.joke_list)
+        print("hello, I'm jokebot!")
+        print("Type 'next' to hear a joke :-)")
+        while self.read_input():
+            try:
+                joke = next(joke_spitter)
+                self.tell_joke(joke[0], joke[1])
+            except:
+                print("sorry! out of jokes")
+
+    def use_reddit(self):
+        r = requests.get("https://www.reddit.com/r/dadjokes.json", headers = {'User-agent': 'your bot 0.1'})
+        data = r.json()
+        jokes = []
+        for joke in data['data']['children']:
+            prompt = joke['data']['title']
+            punchline = joke['data']['selftext']
+            if not joke['data']['over_18'] and self.valid_question(joke['data']['title']):
+                jokes.append([prompt, punchline])
+        return jokes
+
+    def valid_question(self, prompt):
+        lower = prompt.lower()
+        return lower.startswith("what") or lower.startswith("how") or lower.startswith("why")
+if __name__ == "__main__":
+    
+    inputs = sys.argv
+
+    if len(inputs) == 1:
+        Jokebot()
+        
+    elif len(inputs) == 2:
+        Jokebot(inputs[1])
+    
+    else:
+        print('invalid input. Please input a valid jokes file.')
